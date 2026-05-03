2eaabd62c9a3302fcb6b912669547b5c5b011413
Himasree <himasree2507@gmail.com>
2026-05-03 14:41:32 +0530

    Hardcode Supabase connection to fix Render issue

--- final_backend/src/config/database.js
+++ final_backend/src/config/database.js
@@ -1,7 +1,9 @@
 const { Pool } = require('pg');
 
+const SUPABASE_URL = 'postgresql://postgres.aiucznsyjaubfpjjlebd:ewgfehbvrhy4eye@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres';
+
 const pool = new Pool({
-  connectionString: process.env.SUPABASE_CONN || process.env.DATABASE_URL,
+  connectionString: SUPABASE_URL,
   ssl: {
     rejectUnauthorized: false
   },
